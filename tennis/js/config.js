aeb65b0bd6df330167b9e9e05bb6eee0bcffbf02
100Rubley <toxa_98@inbox.ru>
2021-01-07 22:20:52 +0300

    feat: add config file

---
+++ tennis/js/config.js
@@ -0,0 +1,15 @@
+export default {
+  // Ball settings
+  ballX: 50,
+  ballY: 50,
+  ballSpeedX: 5,
+  ballSpeedY: 4,
+  // Player settings
+  playerPaddleHeight: 100,
+  playerPaddleThickness: 10,
+  playerPaddleY: 250,
+  // Player settings
+  computerPaddleHeight: 100,
+  computerPaddleThickness: 10,
+  computerPaddleY: 250,
+}
