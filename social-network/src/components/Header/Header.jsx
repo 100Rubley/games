c5b51b8e88cb745c46df635f3820468ee9b10e6e
100Rubley <toxa_98@inbox.ru>
2021-01-26 20:11:52 +0300

    feat: add header style

--- social-network/src/components/Header/Header.jsx
+++ social-network/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import s from './Header.module.css'
+import Icons from './Icons/Icons'
+import Profile from './Profile/Profile.jsx'
 
 const Header = () => {
   return (
     <div className = {s.wrapper}>
-      <div className = {s.item}>Profile</div>
-      <div className = {s.item}>Messeges</div>
-      <div className = {s.item}>News</div>
-      <div className = {s.item}>Music</div>
-      <div className = {s.item}>Settings</div>
+      <Profile />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
     </div>
   )
 }
---
+++ social-network/src/components/Header/Icons/Icons.jsx
@@ -0,0 +1,11 @@
+import s from './Icons.module.css'
+
+const Icons = () => {
+  return (
+    <div className = {s.wrapper}>
+      <img src="#" alt="Icon"/>
+    </div>
+  )
+}
+
+export default Icons
