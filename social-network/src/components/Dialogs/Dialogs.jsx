a34cc923a294a9b898238fea691ca6b7ac21c028
100Rubley <toxa_98@inbox.ru>
2021-02-04 15:37:39 +0300

    refactor: add data base to dialogs component

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -3,20 +3,32 @@ import s from "./Dialogs.module.css"
 import Message from "./Message/Message"
 
 const Dialogs = () => {
+
+  let dialogs = [
+    {id: 1, name: 'Vova'},
+    {id: 2, name: 'Goga'},
+    {id: 3, name: 'Lola'},
+    {id: 4, name: 'Sosa'},
+    {id: 5, name: 'UwU'},
+  ]
+
+  let messages = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: "How're you doing?"},
+    {id: 3, message: "What's next?"},
+  ]
+
+  let dialogElement = dialogs.map( d => <DialogItem name = {d.name} id = {d.id} />)
+  let messagesElement = messages.map( m => <Message message={m.message} />)
+
   return (
     <div className={s.wrapper}>
       <div className={s.dialogList}>
-        <DialogItem name = 'Vova' id = '1' />
-        <DialogItem name = 'Goga' id = '2' />
-        <DialogItem name = 'Lola' id = '3' />
-        <DialogItem name = 'Sosa' id = '4' />
-        <DialogItem name = 'UwU' id = '5' />
+        {dialogElement}
       </div>
 
       <div className={s.messageList}>
-        <Message message='Hi' />
-        <Message message="How're you doing?" />
-        <Message message="What's next?" />
+        {messagesElement}
       </div>
     </div>
   )
