2a00ac4c31c01a5b4e87958912024b7c17cdc247
100Rubley <toxa_98@inbox.ru>
2021-02-14 12:16:01 +0300

    feat: add REDUX

--- social-network/src/redux/dialogsReducer.js
+++ social-network/src/redux/dialogsReducer.js
@@ -1,7 +1,19 @@
 const SEND_MESSAGE = 'SEND-MESSAGE'
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [{ id: 1, name: 'Vova' },
+    { id: 2, name: 'Goga' },
+    { id: 3, name: 'Lola' },
+    { id: 4, name: 'Sosa' },
+    { id: 5, name: 'UwU' },],
+    messages: [{ id: 1, message: 'Hi' },
+    { id: 2, message: "How're you doing?" },
+    { id: 3, message: "What's next?" },],
+    newMessageBody: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SEND_MESSAGE:
       let newMessage = {
--- social-network/src/redux/profileReducer.js
+++ social-network/src/redux/profileReducer.js
@@ -1,7 +1,15 @@
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const ADD_POST = 'ADD-POST'
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [{ id: 1, text: 'Hello, watcha doin?' },
+    { id: 2, text: 'How are you?' },
+    { id: 3, text: 'Sehr god, bitte' }
+    ],
+    newPostText: ''
+}
+
+const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST:
       let newPost = {
