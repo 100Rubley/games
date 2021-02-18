05ca1cac4acbfb13f101f76f6f09b97d4058d4a2
100Rubley <toxa_98@inbox.ru>
2021-02-18 15:23:47 +0300

    feat: add new style and complite header

--- social-network/src/components/Header/Header.jsx
+++ social-network/src/components/Header/Header.jsx
@@ -1,17 +1,54 @@
 import s from './Header.module.css'
-import Icons from './Icons/Icons'
 import Profile from './Profile/Profile.jsx'
+import { NavLink } from 'react-router-dom'
+import { BsEnvelopeOpenFill } from 'react-icons/bs'
+import { ImUsers, ImNewspaper } from 'react-icons/im'
+import { BiHeadphone } from 'react-icons/bi'
+import { GiConsoleController } from 'react-icons/gi'
+import { FaPlay } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div className = {s.wrapper}>
-      <Profile link = '/profile'/>
-      <Icons link = '/users'/>
-      <Icons link = '/dialogs'/>
-      <Icons link = '/news'/>
-      <Icons link = '/music'/>
-      <Icons link = '/videos'/>
-      <Icons link = '/games'/>
+    <div className={s.wrapper}>
+      <div className={s.wrapperContent}>
+        <Profile link='/profile' />
+
+        <NavLink to='/dialogs' activeClassName={s.activeLink}>
+          <div className={s.iconWrapper}>
+            <BsEnvelopeOpenFill size='1.8em'/>
+          </div>
+        </NavLink>
+
+        <NavLink to='/users' activeClassName={s.activeLink}>
+          <div className={s.iconWrapper}>
+            <ImUsers size='1.8em'/>
+          </div>
+        </NavLink>
+
+        <NavLink to='/news' activeClassName={s.activeLink}>
+          <div className={s.iconWrapper}>
+            <ImNewspaper size='1.8em'/>
+          </div>
+        </NavLink>
+
+        <NavLink to='/music' activeClassName={s.activeLink}>
+          <div className={s.iconWrapper}>
+            <BiHeadphone size='1.8em'/>
+          </div>
+        </NavLink>
+
+        <NavLink to='/videos' activeClassName={s.activeLink}>
+          <div className={s.iconWrapper}>
+            <FaPlay size='1.4em'/>
+          </div>
+        </NavLink>
+
+        <NavLink to='/games' activeClassName={s.activeLink}>
+          <div className={s.iconWrapper}>
+            <GiConsoleController size='1.8em'/>
+          </div>
+        </NavLink>
+      </div>
     </div>
   )
 }
